35ca3efd66f757fa9f69ddca719cbc2b0a00f250
Maxim <haxpeha1990@gmail.com>
2019-05-16 19:34:29 +0300

    Change updateShouldNotify in InheritedAuth

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/home/home.dart';
-import 'ui/pages/screen.dart';
-import 'utils/route_transition.dart';
 import 'pallete.dart';
 import 'typography.dart';
+import 'ui/pages/home/home.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '4u.house',
-      theme: ThemeData(
-          accentColor: accentColor,
-          primaryColor: primaryColor,
-          textTheme: customTextTheme),
-      home: HomePage(),
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case '/home':
-            return SlideRoute(widget: HomePage(),side: 'left');
-            break;
-          default:
-            return SlideRoute(widget: Screen(settings.name));
-        }
-      },
-    );
+        title: '4u.house',
+        theme: ThemeData(
+            accentColor: accentColor,
+            primaryColor: primaryColor,
+            textTheme: customTextTheme),
+        home: HomePage());
   }
 }
