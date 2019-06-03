6ec8e15851637f1ba493d7b1c141c755e6148c71
Maxim <haxpeha1990@gmail.com>
2019-06-03 12:24:39 +0300

    PhoneApi

--- lib/src/resources/phone_repository.dart
+++ lib/src/resources/phone_repository.dart
@@ -3,10 +3,12 @@ import '../models/country_phone_data.dart';
 import 'api/phone_api.dart';
 
 class PhoneRepository {
+  PhoneApi phoneApi = PhoneApi();
+
   Future<List<CountryPhoneData>> getCountriesPhoneData() async {
     final List<CountryPhoneData> data = List<CountryPhoneData>();
-    data.add(CountryPhoneData.fromJson(
-        await PhoneApi().requestCountriesPhoneData()));
+    data.add(
+        CountryPhoneData.fromJson(await phoneApi.requestCountriesPhoneData()));
     return data;
   }
 }
