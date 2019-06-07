aa85c29ca9ab64a0da568e5a188d216b5ba7fff4
Vadim popov <m.vadimpopov@gmail.com>
2019-06-07 11:59:22 +0300

    1. Added bloc.dispatch in go back

--- lib/src/ui/pages/login/otp.dart
+++ lib/src/ui/pages/login/otp.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_mobile/src/resources/phone_repository_test.dart';
 import 'package:user_mobile/src/ui/components/pickers/phone/phone_picker.dart';
-import '../../../blocs/phone/phone_bloc.dart';
-import '../../../blocs/phone/phone_event.dart';
-import '../../../blocs/phone/phone_state.dart';
+import '../../../blocs/login/login_bloc.dart';
+import '../../../blocs/login/login_event.dart';
+import '../../../blocs/login/login_state.dart';
 import '../../../models/country_phone_data.dart';
 import '../../components/common/page_template.dart' show PageTemplate;
 import '../../components/common/snackbar.dart';
@@ -21,9 +21,14 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  LoginBloc _bloc;
+
   @override
   Widget build(BuildContext context) {
     return PageTemplate(
+        goBack: () {
+          _bloc.dispatch(CodeEnteringCanceled(widget.phone));
+        },
         title: 'Confirm',
         body: Container(
           padding: const EdgeInsets.only(left: 14.0, right: 14.0),
