eaee4400165ef38ccd8248a03e2986ec56a771ae
Maxim <kos.maksim90@mail.ru>
2019-06-10 18:37:23 +0300

    isInput Update

--- lib/src/blocs/phone/phone_bloc.dart
+++ lib/src/blocs/phone/phone_bloc.dart
@@ -18,20 +18,21 @@ class PhoneBloc extends Bloc<PhoneEvent, PhoneState> {
     if (event is PhoneInitialized) {
       yield PhoneLoading();
       try {
-        final List<CountryPhoneData> data =
-            await repository.getCountriesPhoneData();
-        final String countryCode = await repository.getCountryByIp();
-        data.sort((CountryPhoneData a, CountryPhoneData b) {
-          if (a.countryId == countryCode) {
-            return -1;
-          }
-          if (b.countryId == countryCode) {
-            return 1;
-          }
-          return a.countryId.compareTo(b.countryId);
+        final List<CountryPhoneData> data = await Future.wait([
+          repository.getCountriesPhoneData(),
+          repository.getCountryByIp()
+        ]).then((List<dynamic> waitList) {
+          waitList[0].sort((CountryPhoneData a, CountryPhoneData b) {
+            if (a.countryId == waitList[1]) {
+              return -1;
+            }
+            if (b.countryId == waitList[1]) {
+              return 1;
+            }
+            return a.countryId.compareTo(b.countryId);
+          });
+          return waitList[0];
         });
-        print(data.length);
-        print('Your countryCode is $countryCode');
         yield PhoneCountriesDataLoaded(data);
       } catch (error) {
         print('=> PhoneState => $error');
