bd484c90ea0f7d98acabf6f5ebc9e3a9520378e8
Vadim Popov <m.vadimpopov@gmail.com>
2019-06-10 17:49:06 +0300

    1. Added Resend component

--- lib/src/ui/pages/login/otp.dart
+++ lib/src/ui/pages/login/otp.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_mobile/src/resources/auth_repository.dart';
-import 'package:user_mobile/src/ui/components/pickers/phone/phone_picker.dart';
 import '../../../blocs/auth/auth_bloc.dart';
 import '../../../blocs/auth/auth_state.dart';
 import '../../../blocs/login/login_bloc.dart';
@@ -13,6 +12,7 @@ import '../../../resources/auth_repository.dart';
 import '../../components/common/page_template.dart' show PageTemplate;
 import '../../components/common/snackbar.dart';
 import '../../components/common/styled_button.dart' show StyledButton;
+import '../../components/common/resend.dart';
 
 class OtpScreen extends StatefulWidget {
   const OtpScreen({@required this.selectedItem, @required this.phone});
@@ -80,83 +80,102 @@ class _OtpScreenState extends State<OtpScreen> {
         body: Container(
           padding: const EdgeInsets.only(left: 14.0, right: 14.0),
           margin: const EdgeInsets.only(bottom: 12.0),
-          child: Column(children: <Widget>[
-            _buildHeadLine(),
-            BlocListener<LoginEvent, LoginState>(
-              bloc: _bloc,
-              listener: (BuildContext context, LoginState state) {
-                print('===> state listener name : ' + state.toString());
-                if (state is PhoneError || state is CodeError) {
-                  Scaffold.of(context).showSnackBar(const CustomSnackBar(
-                    content: Text('Something went wrong'),
-                    backgroundColor: Colors.redAccent,
-                  ));
-                }
-                if (state is CodeError) {
-                  //Очищаешь TextField.
-                  print('CodeError');
-                  code.clear();
-                }
-                if (state is PhoneEntering) {
-                  //Убиваем рут (возвращаемся на экран ввода номера телефона).
-                  print('PhoneEntering');
-                  //Так как это стартовый стейт в LoginBloc роут закрывается при открытии, поэтому закоментил, возможно в постановке ошибка?
-                  //Navigator.pop(context);
-                }
-                if (state is AuthAuthorized) {
-                  //Убиваем оба рута (страница ввода номера телефона, страница ввода sms-кода).
-                  print('AuthAuthorized');
-                  Navigator.popUntil(
-                      context, ModalRoute.withName(Navigator.defaultRouteName));
-                }
-              },
-              child: BlocBuilder<LoginEvent, LoginState>(
-                  bloc: _bloc,
-                  builder: (BuildContext context, LoginState state) {
-                    print('===> state builder name : ' + state.toString());
-                    if (state is PhoneEntering) {
-                      // В постановке этот стейт есть только в BlocListener
-                      return Container();
-                    }
-                    if (state is OtpSent) {
-                      return _buildCodeInput();
-                    }
-                    if (state is IsFetchingOtp) {
-                      //Вместо виджета Resend отображаем индикатор загрузки.
-                      return CircularProgressIndicator(
-                          backgroundColor: Theme.of(context).primaryColor);
-                    }
-                    if (state is IsFetchingCode) {
-                      //Кнопка "Send" задизаблена и показывает индикатор загрузки. Реализовать через styled_button.
-                      isFetchingCode = true;
-                      return _buildCodeInput();
-                    }
-                  }),
-            ),
-            _buildResentCode(),
-            _buildSendButton(),
-          ]),
+          child: BlocListener<LoginEvent, LoginState>(
+            bloc: _bloc,
+            listener: (BuildContext context, LoginState state) {
+              print('===> state listener name : ' + state.toString());
+              if (state is PhoneError || state is CodeError) {
+                Scaffold.of(context).showSnackBar(const CustomSnackBar(
+                  content: Text('Something went wrong'),
+                  backgroundColor: Colors.redAccent,
+                ));
+              }
+              if (state is CodeError) {
+                //Очищаешь TextField.
+                print('CodeError');
+                code.clear();
+              }
+              if (state is PhoneEntering) {
+                //Убиваем рут (возвращаемся на экран ввода номера телефона).
+                print('PhoneEntering');
+                //Так как это стартовый стейт в LoginBloc роут закрывается при открытии, поэтому закоментил, возможно в постановке ошибка?
+                //Navigator.pop(context);
+              }
+              if (state is AuthAuthorized) {
+                //Убиваем оба рута (страница ввода номера телефона, страница ввода sms-кода).
+                print('AuthAuthorized');
+                Navigator.popUntil(
+                    context, ModalRoute.withName(Navigator.defaultRouteName));
+              }
+            },
+            child: BlocBuilder<LoginEvent, LoginState>(
+                bloc: _bloc,
+                builder: (BuildContext context, LoginState state) {
+                  print('===> state builder name : ' + state.toString());
+                  if (state is PhoneEntering) {
+                    // В постановке этот стейт есть только в BlocListener
+                    return Container();
+                  }
+                  if (state is OtpSent) {
+                    return Column(children: <Widget>[
+                      _buildHeadLine(),
+                      _buildCodeInput(),
+                      Resend(
+                          type: 'Timer',
+                          onPressed: () {
+                            _bloc.dispatch(OtpRequested(widget.phone));
+                          }),
+                      _buildSendButton()
+                    ]);
+                  }
+                  if (state is IsFetchingOtp) {
+                    //Вместо виджета Resend отображаем индикатор загрузки.
+                    return Column(children: <Widget>[
+                      _buildHeadLine(),
+                      _buildCodeInput(),
+                      const Resend(type: 'Circular'),
+                      _buildSendButton()
+                    ]);
+                  }
+                  if (state is IsFetchingCode) {
+                    //Кнопка "Send" задизаблена и показывает индикатор загрузки. Реализовать через styled_button.
+                    isFetchingCode = true;
+                    return Column(children: <Widget>[
+                      _buildHeadLine(),
+                      _buildCodeInput(),
+                      Resend(
+                          type: 'Timer',
+                          onPressed: () {
+                            _bloc.dispatch(OtpRequested(widget.phone));
+                          }),
+                      _buildSendButton()
+                    ]);
+                  }
+                }),
+          ),
         ));
   }
 
-  Widget _buildResentCode() {
-    return Container(
-        margin: const EdgeInsets.only(top: 18.0),
-        child: start != 0
-            ? Text('Resend code through 00.$start sec',
-                style: const TextStyle(fontSize: 14, color: Color(0x8a000000)))
-            : InkWell(
-                onTap: () {
-                  _bloc.dispatch(OtpRequested(widget.phone));
-                  setState(() {
-                    startTimer();
-                  });
-                },
-                child: Text('Resent code',
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Theme.of(context).primaryColor))));
-  }
+//  Widget _buildResentCode() {
+//    return Container(
+//        margin: start != 0
+//            ? const EdgeInsets.only(top: 18.0)
+//            : const EdgeInsets.only(top: 2.0),
+//        child: start != 0
+//            ? Text('Resend code through 00.$start sec',
+//                style: const TextStyle(fontSize: 14, color: Color(0x8a000000)))
+//            : FlatButton(
+//                onPressed: () {
+//                  _bloc.dispatch(OtpRequested(widget.phone));
+//                  setState(() {
+//                    startTimer();
+//                  });
+//                },
+//                child: Text('Resent code',
+//                    style: TextStyle(
+//                        fontSize: 14.0,
+//                        color: Theme.of(context).primaryColor))));
+//  }
 
   Widget _buildHeadLine() {
     return Container(
