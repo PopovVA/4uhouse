57ff0fd36b44f7308586f4c78ab9ef0d8b990659
Maxim <kos.maksim90@mail.ru>
2019-06-18 15:53:05 +0300

    Refresh

--- lib/temp/alert_test_dialog.dart
+++ lib/temp/alert_test_dialog.dart
@@ -20,40 +20,45 @@ class StyledAlertDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: InkWell(
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-        child: Center(
-          child: Container(
-              decoration:
-                  BoxDecoration(color: Colors.white, boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 12,
-                    offset: const Offset(0.0, 12.0))
-              ]),
-              width: 280,
-              height: 120,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24, top: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.only(bottom: 12.0),
-                        child: title),
-                    content,
-                    Padding(
-                      padding: const EdgeInsets.only(top: 2.0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: actions),
-                    )
-                  ],
-                ),
-              )),
-        ),
+      body: Stack(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          Align(
+            alignment: FractionalOffset.center,
+            child: Container(
+                decoration:
+                    BoxDecoration(color: Colors.white, boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 12,
+                      offset: const Offset(0.0, 12.0))
+                ]),
+                width: 280,
+                height: 120,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24, right: 24, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 12.0),
+                          child: title),
+                      content,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: actions),
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ],
       ),
     );
   }
