c861d75a8dbf216bbde60d1530343bfd2d10053b
Maxim <kos.maksim90@mail.ru>
2019-06-20 09:55:01 +0300

    Update

--- lib/temp/styled_text_controler.dart
+++ lib/temp/styled_text_controler.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class NumberOnlyTextEditingController extends TextEditingController {
   NumberOnlyTextEditingController() {
-    //
     addListener(() {
       String numberText = '';
       for (String number in text.split('')) {
@@ -17,17 +16,5 @@ class NumberOnlyTextEditingController extends TextEditingController {
         composing: TextRange.empty,
       );
     });
-    //
-   /* addListener(() {
-      if (!RegExp('^[0-9]*[0-9]\$').hasMatch(text)) {
-        text = text.replaceAll(text[text.length - 1], '');
-      }
-      value = value.copyWith(
-        text: text,
-        selection:
-            TextSelection(baseOffset: text.length, extentOffset: text.length),
-        composing: TextRange.empty,
-      );
-    });*/
   }
 }
