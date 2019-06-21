29a4434cb2b6e2f638ca2391e90c3b5ea2a54433
Maxim <kos.maksim90@mail.ru>
2019-06-21 18:29:46 +0300

    Update

--- lib/src/ui/components/styled/styled_alert_dialog.dart
+++ lib/src/ui/components/styled/styled_alert_dialog.dart
@@ -32,67 +32,57 @@ class StyledAlertDialog extends StatelessWidget {
                 ]),
                 width: 280,
                 height: 120,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24, right: 24, top: 10),
-                  child: Stack(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                              padding: const EdgeInsets.only(bottom: 12.0),
-                              child: Text(
-                                title is String ? title : 'Opps...',
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Color.fromRGBO(0, 0, 0, 0.87),
-                                ),
-                              )),
-                          Text(
-                            content is String ? content : null,
-                            style: const TextStyle(
-                              color: Color.fromRGBO(0, 0, 0, 0.541327),
-                              fontSize: 16,
-                            ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.only(bottom: 12.0,left: 24, right: 24, top: 10),
+                        child: Text(
+                          title is String ? title : 'Opps...',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Color.fromRGBO(0, 0, 0, 0.87),
                           ),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(left:24,right: 24,bottom: 8),
+                      child: Text(
+                        content is String ? content : null,
+                        style: const TextStyle(
+                          color: Color.fromRGBO(0, 0, 0, 0.541327),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          if (onCancel is Function)
+                            FlatButton(
+                                onPressed: onCancel,
+                                child: const Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    color: DISABLED_COLOR,
+                                    fontSize: 16,
+                                  ),
+                                )),
+                          if (onOk is Function)
+                            FlatButton(
+                                onPressed: onOk,
+                                child: const Text(
+                                  'OK',
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(55, 180, 188, 1),
+                                    fontSize: 16,
+                                  ),
+                                )),
                         ],
                       ),
-                      Align(
-                        alignment: FractionalOffset.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 2.0, bottom: 12),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              if (onCancel is Function)
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 12),
-                                  child: InkWell(
-                                      onTap: onCancel,
-                                      child: const Text(
-                                        'Cancel',
-                                        style: TextStyle(
-                                          color: DISABLED_COLOR,
-                                          fontSize: 16,
-                                        ),
-                                      )),
-                                ),
-                              if (onOk is Function)
-                                InkWell(
-                                    onTap: onOk,
-                                    child: const Text(
-                                      'OK',
-                                      style: TextStyle(
-                                        color: Color.fromRGBO(55, 180, 188, 1),
-                                        fontSize: 16,
-                                      ),
-                                    )),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                  ],
                 )),
           ),
         ],
