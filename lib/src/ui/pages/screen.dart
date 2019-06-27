a15a09617fb233f5187fcbf17c7e18ee50c50215
Vadim popov <m.vadimpopov@gmail.com>
2019-06-27 14:58:04 +0300

    1. Fixed button padding

--- lib/src/ui/pages/screen.dart
+++ lib/src/ui/pages/screen.dart
@@ -62,9 +62,9 @@ class _ScreenState extends State<Screen> {
   void initState() {
     super.initState();
     screenBloc = ScreenBloc(
-        screenRepository: ScreenRepository(), authRepository: AuthRepository());
-//        screenRepository: TestScreenRepository(),
-//        authRepository: AuthRepository());
+//        screenRepository: ScreenRepository(), authRepository: AuthRepository());
+        screenRepository: TestScreenRepository(),
+        authRepository: AuthRepository());
     scrollToId = widget.scrollToId;
     screenBloc.dispatch(ScreenRequested(query: widget.route));
   }
@@ -153,6 +153,8 @@ class _ScreenState extends State<Screen> {
                 Expanded(
                   child: Container(
                     child: SingleChildScrollView(
+                      padding: buttons.isNotEmpty ? EdgeInsets.only(
+                          bottom: 64 * buttons.length.toDouble()) : null,
                       controller: widget.scrollController,
                       child: Column(
                         children: items,
