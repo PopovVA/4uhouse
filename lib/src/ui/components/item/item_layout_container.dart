f59326c6fa7e5daeb5ad65c18e8b288e92bc3955
Maxim <kos.maksim90@mail.ru>
2019-07-11 12:45:17 +0300

    Correct Item Animation Press

--- lib/src/ui/components/item/item_layout_container.dart
+++ lib/src/ui/components/item/item_layout_container.dart
@@ -10,7 +10,7 @@ class ItemLayoutContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Ink(
-      color: Colors.blue,
+      color: Colors.white,
       child: InkWell(
         onTap: onTap is Function ? onTap : null,
         child: buildContainer(child),
