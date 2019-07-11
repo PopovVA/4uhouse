fd143f13bac4551bf8afd9fb4b1a204654f3c60f
Maxim <kos.maksim90@mail.ru>
2019-07-11 18:28:07 +0300

    Fix GoBack Icon

--- lib/src/ui/components/page_template.dart
+++ lib/src/ui/components/page_template.dart
@@ -5,16 +5,15 @@ import '../components/styled/styled_circular_progress.dart'
     show StyledCircularProgress;
 
 class PageTemplate extends StatelessWidget {
-  const PageTemplate({
-    this.title,
-    this.note,
-    this.body,
-    this.goBack,
-    this.drawer,
-    this.loading = false,
-    this.padding = false,
-    this.actions
-  });
+  const PageTemplate(
+      {this.title,
+      this.note,
+      this.body,
+      this.goBack,
+      this.drawer,
+      this.loading = false,
+      this.padding = false,
+      this.actions});
 
   static const Color color = Color(0xFF585555);
   static const double height = 68.0;
@@ -33,28 +32,27 @@ class PageTemplate extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: const Color(0xFFe9e7e7),
-        iconTheme: const IconThemeData(color: color),
-        leading: loading
-            ? null
-            : drawer == null
-                ? IconButton(
-                    color: color,
-                    tooltip: 'go back',
-                    icon: const Icon(Icons.arrow_back_ios),
-                    onPressed: () {
-                      if (goBack is Function) {
-                        goBack();
-                      } else if (Navigator.canPop(context)) {
-                        Navigator.pop(context);
-                      }
-                    })
-                : null,
-        centerTitle: true,
-        title:
-            Text(title, style: const TextStyle(color: color, fontSize: 20.0)),
-        actions: actions
-      ),
+          backgroundColor: const Color(0xFFe9e7e7),
+          iconTheme: const IconThemeData(color: color),
+          leading: loading
+              ? Container()
+              : drawer == null
+                  ? IconButton(
+                      color: color,
+                      tooltip: 'go back',
+                      icon: const Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        if (goBack is Function) {
+                          goBack();
+                        } else if (Navigator.canPop(context)) {
+                          Navigator.pop(context);
+                        }
+                      })
+                  : null,
+          centerTitle: true,
+          title:
+              Text(title, style: const TextStyle(color: color, fontSize: 20.0)),
+          actions: actions),
       drawer: drawer,
       body: Container(
         child: body,
